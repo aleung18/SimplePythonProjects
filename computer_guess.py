05ac8c8d5a5a5a47c0321b51ab9e24a6182854a8
Ashton Leung <96746911+aleung18@users.noreply.github.com>
2022-07-11 17:43:41 -0400

    Update computer_guess.py

--- computer_guess.py
+++ computer_guess.py
@@ -1,57 +1,29 @@
 # user generates random number that computer has to guess
 
-# personal version
+# video version
 
 from random import randint
 
-lower = 1
-upper = int(input("Please enter upper bound: "))
-feedback = ""
-num_tries = 0
-
-while feedback != "c":
-    
-    if lower != upper:
-        computer_guess = randint(lower, upper)
-    else:
-        computer_guess = lower # can also be upper because they are same value
-    
-    feedback = input(f"Is my guess ({computer_guess}) too low (L), too high (H), or correct (C)? ").lower()
-    if feedback == "c":
-        num_tries += 1
-        print(f"Yay! I guessed the number ({computer_guess}) in {num_tries} tries")
-    elif feedback == "l":
-        num_tries += 1
-        lower = computer_guess + 1
-    elif feedback == "h":
-        num_tries += 1
-        upper = computer_guess - 1
-
-
-# video version
-
-# from random import randint
-
-# def computer_guess(x):
-#     num_tries = 1
-#     lower_bound = 1
-#     upper_bound = x
-#     feedback = ""
-#     while feedback != "c":
-#         if lower_bound != upper_bound:
-#             guess = randint(lower_bound, upper_bound)
-#         else:
-#             num_tries += 1
-#             guess = lower_bound # can also be upper_bound b/c they are the same number in this conditional
+def computer_guess(x):
+    num_tries = 1
+    lower_bound = 1
+    upper_bound = x
+    feedback = ""
+    while feedback != "c":
+        if lower_bound != upper_bound:
+            guess = randint(lower_bound, upper_bound)
+        else:
+            num_tries += 1
+            guess = lower_bound # can also be upper_bound b/c they are the same number in this conditional
         
-#         feedback = input(f"Is {guess} too high (H), too low (L), or correct (C)? ").lower()
-#         if feedback == "h":
-#             num_tries += 1
-#             upper_bound = guess - 1
-#         elif feedback == "l":
-#             num_tries += 1
-#             lower_bound = guess + 1
+        feedback = input(f"Is {guess} too high (H), too low (L), or correct (C)? ").lower()
+        if feedback == "h":
+            num_tries += 1
+            upper_bound = guess - 1
+        elif feedback == "l":
+            num_tries += 1
+            lower_bound = guess + 1
 
-#     return f"Yay! The computer guessed your number ({guess}) in {num_tries} tries"
+    return f"Yay! The computer guessed your number ({guess}) in {num_tries} tries"
 
-# print(computer_guess(10))
+print(computer_guess(10))
