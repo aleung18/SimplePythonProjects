1f27736c6ab344cb50798fe0469505538f2075f3
Ashton Leung <96746911+aleung18@users.noreply.github.com>
2022-07-11 17:45:44 -0400

    Update user_guess.py

--- user_guess.py
+++ user_guess.py
@@ -3,38 +3,18 @@
 
 from random import randint
 
-# personal version
-
-random_number = randint(1, 9)
-
-user_guess = ""
-num_guesses = 0
-
-while(user_guess != random_number):
-    user_guess = int(input("Enter a number: "))
-    if user_guess == random_number:
-        num_guesses += 1
-        print(f"Congrats! You got the number in {num_guesses} tries.")
-    elif user_guess < random_number:
-        num_guesses += 1
-        print("Too low!")
-    elif user_guess > random_number:
-        num_guesses += 1
-        print("Too high!")
-
-
 # video version
 
-# def guess(x):
-#     random_number = randint(1, x)
-#     guess = 0
-#     while(guess != random_number):
-#         guess = int(input(f"Guess a number between 1 and {x}: "))
-#         if guess < random_number:
-#             print("Sorry, guess again. Too low")
-#         elif guess > random_number:
-#             print("Sorry, guess again. Too high")
+def guess(x):
+    random_number = randint(1, x)
+    guess = 0
+    while(guess != random_number):
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+        if guess < random_number:
+            print("Sorry, guess again. Too low")
+        elif guess > random_number:
+            print("Sorry, guess again. Too high")
 
-#     print(f"Yay, congrats! You have guessed the number ({random_number})")
+    print(f"Yay, congrats! You have guessed the number ({random_number})")
 
-# guess(10)
+guess(10)
